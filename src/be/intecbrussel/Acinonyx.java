95baf753db0bc8748bfb2125b4fc73bc1a99c798
AxelB0 <axel.boonen.mail@gmail.com>
2022-05-09 15:14:23 +0200

    getter and setter for speed

--- src/be/intecbrussel/Acinonyx.java
+++ src/be/intecbrussel/Acinonyx.java
@@ -4,6 +4,14 @@ public abstract class Acinonyx extends Animal {
 
     private int speed;
 
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
     public void run(double distance) {
 
     }
